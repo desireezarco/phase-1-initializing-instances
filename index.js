d26c69e8567793cacdd7716bb7f11b24a3c4311c
Desiree Zarco <desireeannzarco7@gmail.com>
2023-06-04 14:38:11 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-// Write your code here
+class Breakfast {
+    constructor(food, drink){
+        this.food = food;
+        this.drink = drink;
+        console.log(this)
+    }
+}
+
+new Breakfast('eggs', 'juice')
+
+class Lunch {
+    constructor(salad, soup, drink){
+        this.salad = salad
+        this.soup = soup
+        this.drink = drink
+        console.log(this)
+    }
+}
+
+new Lunch('side salad', 'broccoli cheddar soup', 'iced tea')
+
+class Dinner{
+    #dessert
+    constructor(salad, soup, entree, dessert){
+        this.salad = salad
+        this.soup = soup
+        this.entree = entree
+        this.#dessert = dessert
+        console.log(this)
+    }
+}
+
+new Dinner('balsamic salad', 'consomme', 'filet mignon', 'cheesecake')
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Cat{
+//     constructor(name, breed, age){
+//         this.name = name
+//         this.breed = breed
+//         this.age = age
+//         console.log(this)
+//     }
+// }
+
+// new Cat('rose', 'domestic longhair', 9)
